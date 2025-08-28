c1842e7b6814244339d888ea46107e0a62a15148
Nilo Tengman <nilo.tengman@elev.ga.ntig.se>
2025-08-28 12:56:05 +0200

    klar, ending, dmg, namn.

--- repetition/Program.cs
+++ repetition/Program.cs
@@ -10,20 +10,28 @@ static void Fight()
     int p1Dmg = 0;
     int p2Dmg = 0;
 
-    string p1Name = "Hugo";
-    string p2Name = "Bevdar";
+    string p1Name = ("");
+    string p2Name = ("");
 
 
 
-    Console.WriteLine($"{p1Name}: {p1Hp}");
-    Console.WriteLine($"{p2Name}: {p2Hp}");
-    Console.WriteLine("_____________________");
+    Console.WriteLine("Vad heter din goat");
+    p1Name = Console.ReadLine();
+    Console.WriteLine("Vad heter din opp");
+    p2Name = Console.ReadLine();
+    Console.WriteLine("");
+
+
+
+    Console.WriteLine($"{p1Name}: {p1Hp} hp");
+    Console.WriteLine($"{p2Name}: {p2Hp} hp");
+    Console.WriteLine("");
 
     while (p1Hp > 0 && p2Hp > 0)
     {
 
-        p1Dmg = Random.Shared.Next(10, 26);
-        p2Dmg = Random.Shared.Next(10, 26);
+        p1Dmg = Random.Shared.Next(1, 26);
+        p2Dmg = Random.Shared.Next(1, 26);
         p1Hp -= p1Dmg;
         p2Hp -= p2Dmg;
         Console.WriteLine($"{p1Name}:gjorde {p1Dmg} Dmg");
@@ -38,15 +46,15 @@ static void Fight()
 
     if (p1Hp > 0)
     {
-        Console.WriteLine($"{p1Name} vann!");
+        Console.WriteLine($"{p1Name} vann! Han slaktade den opp {p2Name}, och rädda världen elle nåt sånt.");
     }
     else if (p1Hp <= 0 && p2Hp <= 0)
     {
-        Console.WriteLine("Oavgjort!");
+        Console.WriteLine($"Både din opp {p2Name} och din goat {p1Name} är döda. Vi vann, men var det värt det? Fs in the chat please.");
     }
     else
     {
-        Console.WriteLine($"{p2Name} vann!");
+        Console.WriteLine($"{p2Name} vann :o , nu kommer din opp nog döda alla du älskar :p");
     }
 
     Console.ReadLine();
