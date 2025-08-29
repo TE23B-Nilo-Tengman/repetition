763ef2a91ea6b842372d8e0c01f86ff35c4f4b7e
Nilo Tengman <nilo.tengman@elev.ga.ntig.se>
2025-08-29 13:07:18 +0200

    items!!!

--- repetition/Program.cs
+++ repetition/Program.cs
@@ -10,18 +10,34 @@ static void Fight()
     int p1Dmg = 0;
     int p2Dmg = 0;
 
-    string p1Name = ("");
-    string p2Name = ("");
+
 
 
 
     Console.WriteLine("Vad heter din goat");
-    p1Name = Console.ReadLine();
+    string p1Name = Console.ReadLine();
     Console.WriteLine("Vad heter din opp");
-    p2Name = Console.ReadLine();
+    string p2Name = Console.ReadLine();
     Console.WriteLine("");
 
 
+
+    Console.WriteLine($"Vill du att {p1Name} ha en Kniv som ger +5 dmg (Skriv (k) ) eller en sköld som ger plus 50 hp (skriv (s) )???");
+    Console.WriteLine($"om du inte skriver k eller s så får du inget :P");
+    string p1Acc = Console.ReadLine();
+    Console.WriteLine($"Vill du att {p2Name} ha en Kniv som ger +5 dmg (Skriv (k) ) eller en sköld som ger plus 50 hp (skriv (s) )???");
+    string p2Acc = Console.ReadLine();
+
+    if (p1Acc == "s")
+    {
+        p1Hp += 50;
+    }
+    else{}
+    if (p2Acc == "s")
+    {
+        p2Hp += 50;
+    }
+    else{}
 
     Console.WriteLine($"{p1Name}: {p1Hp} hp");
     Console.WriteLine($"{p2Name}: {p2Hp} hp");
@@ -32,15 +48,39 @@ static void Fight()
 
         p1Dmg = Random.Shared.Next(1, 26);
         p2Dmg = Random.Shared.Next(1, 26);
-        p1Hp -= p1Dmg;
-        p2Hp -= p2Dmg;
+
+        if (p1Acc == "k")
+        {
+            p2Hp -= p1Dmg + 5;
+        }
+        else
+        {
+            p2Hp -= p1Dmg;
+        }
+
+
+
+        if (p2Acc == "k")
+        {
+            p1Hp -= p2Dmg + 5;
+        }
+        else
+        {
+            p1Hp -= p2Dmg;
+        }
+
+
+
         Console.WriteLine($"{p1Name}:gjorde {p1Dmg} Dmg");
         Console.WriteLine($"{p2Name}:gjorde {p2Dmg} Dmg");
 
-        Console.WriteLine("_____________________");
+        Console.WriteLine("");
 
         Console.WriteLine($"{p1Name}:har {p1Hp} hp");
         Console.WriteLine($"{p2Name}:har {p2Hp} hp");
+
+        Console.WriteLine("_____________________");
+
         Console.ReadLine();
     }
 
